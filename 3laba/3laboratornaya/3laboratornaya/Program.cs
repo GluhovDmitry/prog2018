a4a7c85c1f80e275d9d3079cacd7efe6b5a5dcbb
Dima <36917381+GluhovDmitry@users.noreply.github.com>
2018-03-31 17:26:46 +0500

    Add files via upload

---
+++ 3laba/3laboratornaya/3laboratornaya/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratornaya2
+{
+    public class Program
+    {
+        public static void QuickSort(int[] array, int low, int high)
+        {
+            if (high == low || array.Length == 0) return;
+            var stick = array[high];
+            var storeIndex = low;
+            for (int i = low; i <= high - 1; i++)
+                if (array[i] <= stick)
+                {
+                    var buf = array[i];
+                    array[i] = array[storeIndex];
+                    array[storeIndex] = buf;
+                    storeIndex++;
+                }
+            var n = array[storeIndex];
+            array[storeIndex] = array[high];
+            array[high] = n;
+            if (storeIndex > low) QuickSort(array, low, storeIndex - 1);
+            if (storeIndex < high) QuickSort(array, storeIndex + 1, high);
+        }
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+        static int[] MakeArray(int lngth)
+        {
+            var array = new int[lngth];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(0, 10);
+            return array;
+        }
+        public static void Main()
+        {
+            var array = MakeArray(5);
+            QuickSort(array);
+            foreach (var value in array)
+                Console.WriteLine(value);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3laba/3laboratornaya/QuickSort.Test/QuickSortTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using laboratornaya2;
+namespace QuickSort.Test
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        //1
+        [TestMethod]
+        public void Test3ElementsArray()
+        {
+            bool value = false;
+            var array = new int[3];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(0, 10);
+            Program.QuickSort(array);
+            if (array[0] <= array[1] && array[1] <= array[2])
+                value = true;
+            Assert.AreEqual(value, true);
+        }
+        //2
+        [TestMethod]
+        public void Test100Elements()
+        {
+            bool value = false;
+            var array = new int[100];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(0, 10);
+            Program.QuickSort(array);
+            foreach (int e in array)
+                if (array[e] <= array[e + 1])
+                    value = true;
+            Assert.AreEqual(value, true);
+        }
+        //3
+        [TestMethod]
+        public void Tes1000ElementsArray()
+        {
+            int value = 0;
+            var k = 0;
+            var array = new int[1000];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(0, 10);
+            Program.QuickSort(array);
+            while (k <= 9) 
+            {
+                var num1 = rnd.Next(0, 998); //первое число пары
+                var num2 = rnd.Next(num1, 999); //второе число пары
+                if (array[num2] >= array[num1])
+                {
+                    value++; //счетчик пар, удовлетворяющих условию
+                }
+                k++; //счетчик пар
+            }
+            Assert.AreEqual(value, k);
+        }
+        //4
+        [TestMethod]
+        public void TestEmptyArray()
+        {
+            bool value = false;
+            var array = new int[0];
+            Program.QuickSort(array);
+            foreach (int e in array)
+                if (array[e] <= array[e + 1])
+                    value = true;
+            Assert.AreEqual(value, false);
+        }
+        //меньше 8 гб оперативы
+    }
+}
