555003f34b8091a517ef0210d6b1318db7c3e5a6
Dima <36917381+GluhovDmitry@users.noreply.github.com>
2018-04-22 15:25:46 +0500

    Add files via upload

---
+++ 4laba/4laba/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Hashtable;
+
+namespace _4laba
+{
+    public class HashTable
+    {
+        static void Main()
+        {
+
+        }
+        class KeyValuePair
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+        List<List<KeyValuePair>> list;
+        public HashTable(int size)
+        {
+            list = new List<List<KeyValuePair>>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new List<KeyValuePair>());
+            }
+        }
+        public void PutPair(object key, object value)
+        {
+            var bucketNo = GetBucketNumber(key);
+            foreach (var keyValuePair in list[bucketNo])
+            {
+                if (Equals(keyValuePair.Key, key))
+                {
+                    keyValuePair.Value = value;
+                    return;
+                }
+            }
+            list[bucketNo].Add(new KeyValuePair { Key = key, Value = value });
+        }
+        public object GetValueByKey(object key)
+        {
+            var BucketNo = GetBucketNumber(key);
+            foreach (var keyValuePair in list[BucketNo])
+            {
+                if (Equals(keyValuePair.Key, key))
+                {
+                    return keyValuePair.Value;
+                }
+            }
+            return null;
+        }
+        private int GetBucketNumber(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % list.Count;
+        }       
+    }
+}
+
---
+++ 4laba/HashTable.Tests/HashTable.Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTable.Tests
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        //Добавление трёх элементов, поиск трёх элементов
+        public void ThreeElementsTest()
+        {
+            var hashTable = new _4laba.HashTable(3);
+            hashTable.PutPair("1", "Petya");
+            hashTable.PutPair("3", "Sasha");
+            hashTable.PutPair("6", "Lesha");
+            Assert.AreEqual(hashTable.GetValueByKey("1"), "Petya");
+            Assert.AreEqual(hashTable.GetValueByKey("3"), "Sasha");
+            Assert.AreEqual(hashTable.GetValueByKey("6"), "Lesha");
+        }
+        //Добавление одного и того же ключа дважды с разными значениями сохраняет последнее добавленное значение
+        [TestMethod]
+        public void EqualKeysTest()
+        {
+            var hashTable = new _4laba.HashTable(3);
+            hashTable.PutPair("1", "Petya");
+            hashTable.PutPair("1", "Vasya");
+            Assert.AreEqual(hashTable.GetValueByKey("1"), "Vasya");
+        }
+        //Добавление 10000 элементов в структуру и поиск одного из них
+        [TestMethod]
+        public void TenThousandElements()
+        {
+            var hashTable = new _4laba.HashTable(10000);
+            for (int i = 0; i < 10000; i++) 
+            {
+                hashTable.PutPair(i, i+"/");
+            }
+            Assert.AreEqual(hashTable.GetValueByKey(12), "12/");
+        }
+
+
+        //Добавление 10000 элементов в структуру и поиск 1000 недобавленных ключей, поиск которых должен вернуть null
+        [TestMethod]
+        public void TenThousandElementsAndThousandNulls()
+        {
+            var hashTable = new _4laba.HashTable(10000);
+            for (int i = 0; i < 9000; i++)
+            {
+                hashTable.PutPair(i, i + "/");
+            }
+            Assert.AreEqual(hashTable.GetValueByKey(12), "12/");
+            Assert.AreEqual(hashTable.GetValueByKey(9001), null);
+        }
+    }
+}
